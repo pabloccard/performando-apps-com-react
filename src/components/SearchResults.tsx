98962ea00b97136a84b303ae7d63cef558fd664a
pabloccard <pabloccard@gmail.com>
2023-02-28 21:36:10 -0300

    memo

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,22 @@
+import { ProductItem } from "./ProductItem"
+
+type SearchResultsProps = {
+    results: Array<{
+        id: number
+        price: number
+        title: string
+    }>
+}
+
+
+export const SearchResults = ({results}: SearchResultsProps) => {
+    return (
+        <div>
+            {results.map(product => {
+                return (
+                    <ProductItem key={product.id} product={product}/>
+                )
+            })}
+        </div>
+    )
+}
